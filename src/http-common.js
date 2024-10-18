c7260d4319ff58cb9bef10fa976d714cf30f3de4
hoihmy <myhoih@kms-technology.com>
2024-10-18 18:12:43 +0700

    Update the baseURL of axios

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://localhost:8080/api"
+      : `http://${window.location.hostname}:8080/api`,
   headers: {
-    "Content-type": "application/json"
-  }
+    "Content-type": "application/json",
+  },
 });
